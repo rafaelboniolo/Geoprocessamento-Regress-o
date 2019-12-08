1ffad2a175bbb9fa031f728520d17411e041b0f4
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-12-08 11:01:44 -0300

    Builder para construir caminho relativo aos arquivos de base de dados

--- src/util/path_builder.py
+++ src/util/path_builder.py
@@ -3,4 +3,8 @@ import os
 def getData():
     return os.path.dirname(__file__).replace('\\util', '\\data')+("\\data.csv")
 
-getData()
+def getDataCamada():
+    return os.path.dirname(__file__).replace('\\util', '\\data')+("\\ctb0022-camada.csv")
+
+def getDataObservacao():
+    return os.path.dirname(__file__).replace('\\util', '\\data')+("\\ctb0022-observacao.csv")
